995a1809e3850c40570d442880178a9cdfdc3ec3
Mahmoud Skafi <mahmoudskafi277@gmail.com>
2020-12-02 20:03:56 +0200

    Add route information 📺

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -5,7 +5,16 @@ house = require('./house-router');
 //just for testing
 router.get('/', (req, res) => {
     res.json({
-        message: "Hello World From Express"
+        message: `
+        /house get request return house information as json
+        /house post requst create new house 
+        /house/:id/l/[boolen] post requst change light status[on,off] 
+        /house/:id/d/[boolen] post requst change door status[on,off] 
+        /house/:id/t/[number] post requst change door status[1,2,3...etc] 
+        /house/:id/v get requst return voiceprints 
+        /house/:id/v post requst add new voiceprint 
+
+        `
     })
 });
 //Router's
